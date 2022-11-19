59cf00e187b7d4484a07f8eec801e4457dbcff19
wwwonka <je.mange.du.boeuf@gmail.com>
2022-11-19 17:34:17 +0100

    Added sound
    Rain God by Hermano Guiterez and some hissing noise layered on top of eachother

--- src/World/components/mixtape/setupModel.js
+++ src/World/components/mixtape/setupModel.js
@@ -4,6 +4,9 @@ function setupModel(data) {
 	const model = data.scene.children[0];
 	const clip = data.animations[0];
 
+	const song = new Audio("/audio/raingod.mp3");
+	const hissing = new Audio("/audio/hissing-hi.mp3");
+
 	const mixer = new AnimationMixer(model);
 	const action = mixer.clipAction(clip);
 	// action.reset();
@@ -22,9 +25,16 @@ function setupModel(data) {
 
 			if (btn.classList.contains("active")) {
 				action.paused = false;
+				hissing.play();
+				setTimeout(() => {
+					song.play();
+				}, 1800);
 				// action.fadeIn(3);
 			} else {
 				action.halt(0.75);
+
+				song.pause();
+				hissing.pause();
 			}
 		};
 		// action.play();
