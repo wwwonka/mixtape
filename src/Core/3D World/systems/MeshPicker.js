0b8a7311b0829a76c918468da932ce0ddfbeda2d
wwwonka <je.mange.du.boeuf@gmail.com>
2022-12-12 07:37:59 +0100

    Fixed bug where mouseIn animation was triggered

--- src/Core/3D World/systems/MeshPicker.js
+++ src/Core/3D World/systems/MeshPicker.js
@@ -26,88 +26,92 @@ class MeshPicker {
 		// If touch is supported, enable orbit controls right away
 		if (supportsTouch) {
 			world.controls.enabled = true;
-		}
-		// ...And when user touches the scene container, play the animation
-		this.container.addEventListener(
-			"touchstart",
-			(event) => {
-				this.scene.getObjectByName("mixtape").onMouseIn();
-			},
-			false
-		);
-		///////////////////////
+			this.container.addEventListener(
+				"touchstart",
+				(event) => {
+					this.scene.getObjectByName("mixtape").onMouseIn();
+				},
+				false
+			);
+		} else {
+			// ...And when user touches the scene container, play the animation
 
-		// Otherwise, change behviour for mouse (desktop)
-		this.container.addEventListener(
-			"pointermove",
-			(event) => {
-				// Set the Raycaster
-				raycaster.setFromCamera(
-					{
-						x:
-							(event.clientX /
-								this.renderer.domElement.clientWidth) *
-								2 -
-							1,
-						y:
-							-(
-								event.clientY /
-								this.renderer.domElement.clientHeight
-							) *
-								2 +
-							1,
-					},
-					this.camera
-				);
+			///////////////////////
 
-				intersects = raycaster.intersectObjects(
-					this.pickableObjects,
-					false
-				);
+			// Otherwise, change behviour for mouse (desktop)
+			this.container.addEventListener(
+				"pointermove",
+				(event) => {
+					// Set the Raycaster
+					raycaster.setFromCamera(
+						{
+							x:
+								(event.clientX /
+									this.renderer.domElement.clientWidth) *
+									2 -
+								1,
+							y:
+								-(
+									event.clientY /
+									this.renderer.domElement.clientHeight
+								) *
+									2 +
+								1,
+						},
+						this.camera
+					);
 
-				if (intersects.length > 0) {
-					// Everything in this conditional block
-					// will be called once only:
-					if (!hasIntersected) {
-						hasIntersected = true;
-						hasExit = false;
+					intersects = raycaster.intersectObjects(
+						this.pickableObjects,
+						false
+					);
 
-						// Change cursor style to signify model can be rotated
-						if (!pointerDown) {
-							document.body.style.cursor = "grab";
-						}
+					if (intersects.length > 0) {
+						// Everything in this conditional block
+						// will be called once only:
+						if (!hasIntersected) {
+							hasIntersected = true;
+							hasExit = false;
 
-						this.intersectedObject = intersects[0].object;
-						intersectedObject = this.intersectedObject;
-
-						console.log("The mesh is under the cursor");
-
-						world.controls.enabled = true;
-
-						this.addPointerEventListeners();
-					}
-				} else {
-					if (hasIntersected) {
-						if (!hasExit) {
-							console.log("The mesh is outside the cursor");
-
+							// Change cursor style to signify model can be rotated
 							if (!pointerDown) {
-								document.body.style.cursor = "default";
+								document.body.style.cursor = "grab";
 							}
+
+							this.intersectedObject = intersects[0].object;
+							intersectedObject = this.intersectedObject;
+
+							console.log("The mesh is under the cursor");
+
+							world.controls.enabled = true;
+
+							this.addPointerEventListeners();
 						}
-						hasIntersected = false;
+					} else {
+						if (hasIntersected) {
+							if (!hasExit) {
+								console.log("The mesh is outside the cursor");
 
-						this.intersectedObject = null;
-					}
+								// Except if user was already dragging the mesh put back the mouse cursor to default
+								if (!pointerDown) {
+									document.body.style.cursor = "default";
+									world.controls.enabled = false;
+								}
+							}
+							hasIntersected = false;
 
-					if (this.intersectedObject === null) {
+							this.intersectedObject = null;
+						}
+
+						if (this.intersectedObject === null) {
+						}
 					}
-				}
-				// this.pickableObjects.forEach((object, intersected) => {
-				// });
-			},
-			false
-		);
+					// this.pickableObjects.forEach((object, intersected) => {
+					// });
+				},
+				false
+			);
+		}
 	}
 
 	addPointerEventListeners() {
@@ -126,9 +130,12 @@ function pointerDownHandler() {
 	console.log("Pointer down");
 	pointerDown = true;
 
-	document.body.style.cursor = "grabbing";
-
-	intersectedObject.parent.onMouseIn();
+	// If mouse intersects the model
+	if (intersects.length === 0) {
+	} else {
+		document.body.style.cursor = "grabbing";
+		intersectedObject.parent.onMouseIn();
+	}
 }
 
 function pointerUpHandler() {
@@ -138,6 +145,7 @@ function pointerUpHandler() {
 	if (intersects.length === 0) {
 		console.log("nothing under");
 		document.body.style.cursor = "default";
+		world.controls.enabled = false;
 	} else {
 		console.log("something under");
 		document.body.style.cursor = "grab";
